d25b97541cfc1bb550117c763f243ca221db0c0b
David Tahim <davidtahim@gmail.com>
2024-02-01 16:39:42 -0300

    animação dos objetos e criação das telas

--- src/main/java/com/jogo/Janela.java
+++ src/main/java/com/jogo/Janela.java
@@ -12,17 +12,7 @@ public class Janela extends JFrame {
        tela = new JPanel(){
         @Override
         public void paintComponent(Graphics g) {
-            g.setColor(Color.BLUE);
-            g.drawLine(0, 240, 640, 240);
-            g.drawRect(10, 25, 20, 20);
-            g.drawOval(30,20,40,30);
-            g.setColor(Color.YELLOW);
-            g.drawLine(320, 0, 320, 480);
-            g.fillRect(110, 125, 120, 120);
-            g.fillOval(230,220,240,230);
-
-            g.setColor(Color.RED);
-            g.drawString("Eu seria um ótimo score",5,10);
+            //os elementos aparecem aqui
         }
 
        } ;    
@@ -31,7 +21,7 @@ public class Janela extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(640,480);
         setVisible(true);
-        tela.repaint();
+        
     }
 public static void main(String[] args) {
     new Janela();
